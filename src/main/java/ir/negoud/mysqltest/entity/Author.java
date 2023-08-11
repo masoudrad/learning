6543f3077c7e6c58225b12549da3d37ed71ecb5e
Masoud Rezaei Rad <sm.rezaeirad@gmail.com>
2023-08-11 10:35:30 +0330

    Add Author entity code

--- src/main/java/ir/negoud/mysqltest/entity/Author.java
+++ src/main/java/ir/negoud/mysqltest/entity/Author.java
@@ -1,12 +1,20 @@
 package ir.negoud.mysqltest.entity;
 
 import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
 @Entity
 public class Author {
 	
-	@OneToOne()
+	@Id
 	private Long id;
+	
+	@OneToOne(mappedBy="id")
+	private Long userId;
+	
+	@OneToMany
+	private Book book;
 
 }
